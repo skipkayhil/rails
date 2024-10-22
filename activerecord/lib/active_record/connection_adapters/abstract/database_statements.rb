9dbf5b6b58d03db0ed496bd987fdadbef2b66c20
Hartley McGuire <skipkayhil@gmail.com>
2024-10-22 13:20:45 -0400

    Partial backport of "Preserve isolation level when
    retrying MySQL transactions"
    
    9414ed8adde96998a8b8142af633ff2acc9bf10b
    
    Co-authored-by: Jean Boussier <jean.boussier@gmail.com>

--- activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb
@@ -102,16 +102,16 @@ module ActiveRecord
         select_all(arel, name, binds, async: async).then(&:rows)
       end
 
-      def query_value(sql, name = nil) # :nodoc:
-        single_value_from_rows(query(sql, name))
+      def query_value(...) # :nodoc:
+        single_value_from_rows(query(...))
       end
 
-      def query_values(sql, name = nil) # :nodoc:
-        query(sql, name).map(&:first)
+      def query_values(...) # :nodoc:
+        query(...).map(&:first)
       end
 
-      def query(sql, name = nil) # :nodoc:
-        internal_exec_query(sql, name).rows
+      def query(...) # :nodoc:
+        internal_exec_query(...).rows
       end
 
       # Determines whether the SQL statement is a write query.
